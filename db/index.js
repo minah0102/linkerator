ce2357fb16edea7534c1243ef2f1afd77ab00830
minah0102 <minah0102@gmail.com>
2021-06-14 15:54:32 -0400

    added database

--- db/index.js
+++ db/index.js
@@ -1,13 +1,125 @@
 // Connect to DB
 const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
+const { unstable_renderSubtreeIntoContainer } = require('react-dom');
+const DB_NAME = 'localhost:5432/linkerator-dev';
 const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
 const client = new Client(DB_URL);
 
 // database methods
 
+//links
+async function getLinkById(id) {
+  try {
+    const { rows: [link] } = await client.query(/*sql*/`
+    SELECT * FROM links
+    WHERE id = $1
+    `, [id]);
+    return link;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getLinkWithoutTags() {
+  try {
+    const { rows } = await client.query(/*sql*/`
+            SELECT * FROM links;
+        `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createLink({ url, count, comment, date }) {
+  try {
+    const { rows: [link] } = await client.query(/*sql*/`
+      INSERT INTO links(url, count, comment, date)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [url, count, comment, date]);
+
+    return link;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//tags
+async function getAllTags() {
+  try {
+    const { rows } = await client.query(/*sql*/`
+    SELECT * FROM tags
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getTagsbyId(id) {
+  try {
+    const { rows: [tags] } = await client.query(/*sql*/`
+    SELECT * FROM tags
+    WHERE id = $1
+    `, [id]);
+    return tags;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTag({name}) {
+  try {
+    const { rows: [tag] } = await client.query(/*sql*/`
+      INSERT INTO tags(name)
+      VALUES ($1)
+      RETURNING *;
+    `, [name]);
+
+    return tag;
+  } catch (error) {
+    throw error;
+  }
+}
+
+//link-tags
+async function getLinkTagsbyId(id) {
+  try {
+    const {rows: linkTags } = await client.query(/*sql*/`
+      SELECT *
+      FROM link_tags
+      WHERE id = $1;
+    `, [id]);
+    return linkTags;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addTagsToLink({ linkId, tagId}) {
+  try {
+    const { rows: [newTag] } = await client.query(/*sql*/`
+    INSERT INTO link_tags("linkId", "tagId")
+    VALUES ($1, $2)
+    RETURNING *;
+    `, [linkId, tagId]);
+    return newTag;
+  } catch (error) {
+    throw error;
+  }
+}
+
 // export
 module.exports = {
   client,
   // db methods
+  getLinkById,
+  getLinkWithoutTags,
+  createLink,
+  getAllTags,
+  getTagsbyId,
+  createTag,
+  getLinkTagsbyId,
+  addTagsToLink
 }
--- db/init_db.js
+++ db/init_db.js
@@ -1,7 +1,12 @@
 // code to build and initialize DB goes here
 const {
-  client
+  client,
   // other db methods 
+  createLink,
+  createTag,
+  getLinkWithoutTags,
+  getAllTags,
+  addTagsToLink
 } = require('./index');
 
 // async function buildTables() {
@@ -20,11 +25,11 @@ const {
 async function dropTables() {
   console.log("Dropping All Tables...");
   try {
-    await client.query(`
-    DROP TABLE IF EXISTS link;
-    DROP TABLE IF EXISTS tags;
+    await client.query(/*sql*/`
     DROP TABLE IF EXISTS link_tags;
-    `);
+      DROP TABLE IF EXISTS links;
+      DROP TABLE IF EXISTS tags;
+      `);
     console.log("Finished dropping tables!");
   } catch (error) {
     console.error("Error while dropping tables!");
@@ -35,36 +40,145 @@ async function dropTables() {
 async function createTables() {
   console.log("Building/Creating Tables...");
   try {
-    await client.query(`
-      CREATE TABLE link(
-        click-count INTEGER,
-        comment,
-        date-shared
-      );
-
+    await client.query(/*sql*/`
+        CREATE TABLE links(
+          id SERIAL PRIMARY KEY,
+          url TEXT UNIQUE NOT NULL,
+          count INTEGER DEFAULT 0,
+          comment TEXT NOT NULL,
+          date DATE DEFAULT CURRENT_DATE
+        );
+    `);
+  
+    await client.query(/*sql*/`
       CREATE TABLE tags(
-
-      );
-
-      CREATE TABLE link_tags(
-
+        id SERIAL PRIMARY KEY,
+        name TEXT UNIQUE NOT NULL
       );
     `);
-    console.log("Finished building/creating tables!");
+        
+    await client.query(/*sql*/`
+      CREATE TABLE link_tags(
+        id SERIAL PRIMARY KEY,
+        "linkId" INTEGER REFERENCES links(id),
+        "tagId" INTEGER REFERENCES tags(id),
+        UNIQUE("linkId", "tagId")
+      );
+    `);
   } catch (error) {
     console.error("Error while building/creating tables!");
-  }
-}
-
-async function populateInitialData() {
-  try {
-    // create useful starting data
-  } catch (error) {
     throw error;
   }
 }
 
-buildTables()
-  .then(populateInitialData)
+// async function populateInitialData() {
+//   try {
+//     // create useful starting data
+    
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+async function createInitialLinks() {
+  try {
+    console.log("Starting to create inital links...");
+    const linksToCreate = [
+      {
+        url: "https://google.com",
+        count: 318,
+        comment: "This is still the best search engine I know of.",
+        date: "13 Sept 2021"
+      },
+      {
+        url: "https://youtube.com",
+        count: 555,
+        comment: "This is my favorite video website.",
+        date: "05 Jan 2020"
+      },
+      {
+        url: "https://amazon.com",
+        count: 708,
+        comment: "This is a good shopping website.",
+        date: "04 July 2021"
+      },
+    ]
+    const links = await Promise.all(linksToCreate.map(createLink));
+
+    console.log("links created:");
+    console.log(links);
+
+  } catch (error) {
+    console.error("Error creating inital links!");
+    throw error;
+  }
+}
+
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+    const tagsToCreate = [
+      { name: "search" },
+      { name: "knowledge" },
+      { name: "tool" },
+      { name: "video" },
+      { name: "uploads" },
+      { name: "shopping" }
+    ]
+
+    const tags = await Promise.all(tagsToCreate.map(createTag));
+
+    console.log("Tags created:");
+    console.log(tags);
+  } catch (error) {
+    console.error("Error creating tags!");
+    throw error;
+  }
+}
+
+async function createInitialLinkTags() {
+  try {
+    console.log("Starting to create link-tags...");
+    const [link1, link2, link3] = await getLinkWithoutTags();
+    const [tag1, tag2, tag3, tag4, tag5, tag6] = await getAllTags();
+
+    const linkTagsToCreate = [
+      { linkId: link1.id, tagId: tag2.id },
+      { linkId: link2.id, tagId: tag4.id },
+      { linkId: link3.id, tagId: tag6.id }
+    ]
+
+    const linkTags = await Promise.all(linkTagsToCreate.map(addTagsToLink));
+    console.log("link-tags created:", linkTags);
+  } catch (error) {
+    console.error("Error creating link-tags!");
+    throw error;
+  }
+}
+
+// buildTables()
+//   .then(populateInitialData)
+//   .catch(console.error)
+//   .finally(() => client.end());
+
+async function rebuildDB() {
+  try {
+    client.connect();
+    await dropTables();
+    await createTables();
+    await createInitialLinks();
+    await createInitialTags();
+    await createInitialLinkTags();
+  } catch (error) {
+    console.error("error during rebuildDB")
+    throw error;
+  }
+}
+
+rebuildDB()
   .catch(console.error)
   .finally(() => client.end());
+
+// module.exports = {
+//   rebuildDB
+// };
